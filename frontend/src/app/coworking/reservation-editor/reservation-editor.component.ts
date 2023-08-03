c4ec3943ae6d7b7e1fc1baba9c86b88774f26cec
Ajay Gandecha <ajgandecha@gmail.com>
2023-08-03 10:08:39 -0400

    Add Autocomplete to Reservation Editor Who Input
    This commit adds autocomplete to the reservation editor's who input chip form control.

--- frontend/src/app/coworking/reservation-editor/reservation-editor.component.ts
+++ frontend/src/app/coworking/reservation-editor/reservation-editor.component.ts
@@ -6,12 +6,15 @@
  * @license MIT
  */
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { profileResolver } from 'src/app/profile/profile.resolver';
 import { Profile } from 'src/app/models.module';
+import { Observable, ReplaySubject, debounceTime, filter, mergeMap, startWith } from 'rxjs';
+import { ProfileService } from 'src/app/profile/profile.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-reservation-editor',
@@ -32,12 +35,20 @@ export class ReservationEditorComponent {
 
   });
 
+  public people: Profile[] = [];
+
   /** Store the currently-logged-in user's profile.  */
   public profile: Profile;
 
+  // 'Who' Form Data
+  public userLookup: FormControl = new FormControl();
+  @ViewChild('whoInput') whoInput!: ElementRef<HTMLInputElement>;
+  private filteredUsers: ReplaySubject<Profile[]> = new ReplaySubject();
+  public filteredUsers$: Observable<Profile[]> = this.filteredUsers.asObservable();
 
   constructor(
     private route: ActivatedRoute,
+    private profileService: ProfileService,
     protected formBuilder: FormBuilder,
     protected snackBar: MatSnackBar) {
 
@@ -49,5 +60,36 @@ export class ReservationEditorComponent {
     const form = this.reservationForm;
     // form.get('name')?.addValidators(Validators.required);
 
+    /** Add current user as first value in the form */
+    this.people.push(this.profile);
+
+  }
+
+  /** Actions to complete when the component loads. */
+  public ngOnInit() {
+
+    // Configure the filtered users list based on the form
+    this.filteredUsers$ = this.userLookup.valueChanges.pipe(
+      startWith(''),
+      filter((search: string) => search.length > 2),
+      debounceTime(100),
+      mergeMap((search) => this.profileService.search(search))
+    );
+  }
+
+  /** Handler for selecting an option in the who chip grid. */
+  public onOptionSelected = (event: MatAutocompleteSelectedEvent) => {
+    let user = event.option.value as Profile;
+    if (this.people.filter(e => e.id === user.id).length == 0) {
+      this.people.push(user);
+    }
+    this.whoInput.nativeElement.value = '';
+    this.userLookup.setValue('');
+  }
+
+  /** Handler for selecting an option in the who chip grid. */
+  public onOptionDeselected = (person: Profile) => {
+    this.people.splice(this.people.indexOf(person), 1);
+    this.userLookup.setValue('');
   }
 }
