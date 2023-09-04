f0745e4b88e4506fd02d1f2b2ea79443645ad050
Ajay Gandecha <ajgandecha@gmail.com>
2023-09-03 23:42:12 -0400

    Add Test Demo Data and Fix Tests

--- backend/test/services/core_data.py
+++ backend/test/services/core_data.py
@@ -6,7 +6,8 @@ this module serves as a helper to bring them all in at once.
 
 import pytest
 from sqlalchemy.orm import Session
-from . import organization_data
+from .organization import organization_test_data
+from . import permission_data, role_data, user_data
 
 __authors__ = ["Kris Jordan"]
 __copyright__ = "Copyright 2023"
@@ -14,6 +15,10 @@ __license__ = "MIT"
 
 @pytest.fixture(autouse=True)
 def setup_insert_data_fixture(session: Session):
-    organization_data.insert_fake_data(session)
+    role_data.insert_fake_data(session)
+    user_data.insert_fake_data(session)
+    permission_data.insert_fake_data(session)
+    organization_test_data.insert_fake_data(session)
+    
     session.commit()
     yield
