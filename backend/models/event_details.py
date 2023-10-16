a07e2543ff22d52f20c4d1d58f69e6dcadef51c7
Ajay Gandecha <ajgandecha@gmail.com>
2023-10-16 16:04:55 -0400

    Add NewEvent Model
    This commit adds a new Pydantic model to handle the creation of new events. This is used since new events would not have an organization passed in.

--- backend/models/event_details.py
+++ backend/models/event_details.py
@@ -1,13 +1,18 @@
 from backend.models.event import Event
 from backend.models.organization import Organization
 
-class EventDetails(Event):
+class NewEvent(Event):
     """
-    Model to represent `Event` connections between users and organizations
-    
-    This model is based on the `EventEntity` model, which defines the shape
-    of the `Event` database in the PostgreSQL database
+    Model to represent a new `Event` request, containing an organization ID
+    to be linked to an organization in the PostgreSQL database
     """
     
     organization_id: int
+
+    
+class EventDetails(NewEvent):
+    """
+    Model to represent `Event` connections between users and organizations
+    """
+    
     organization: Organization | None = None
