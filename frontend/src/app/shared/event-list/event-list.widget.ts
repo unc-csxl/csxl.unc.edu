1b973610bf085d4ec772cbe98a65f39cb92dca0a
Ajay Gandecha <ajgandecha@gmail.com>
2023-10-22 12:06:58 -0400

    Create Event List Widget in Shared Module
    This commit creates the Event List widget in the Shared Module so that it can be accessed from both events and organizations.

--- frontend/src/app/shared/event-list/event-list.widget.ts
+++ frontend/src/app/shared/event-list/event-list.widget.ts
@@ -1,13 +1,16 @@
 /**
  * The Event List widget abstracts the implementation of the
  * event list from the page.
+ * 
+ * NOTE: This widget is in the Shared module because it is
+ * used both by events and organizations.
  *
  * @author Ajay Gandecha
  * @copyright 2023
  * @license MIT
  */
 
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Event } from '../../event/event.model';
 
 @Component({
@@ -18,7 +21,15 @@ import { Event } from '../../event/event.model';
 export class EventList {
 
     /** The event for the event card to display */
-    @Input() event!: Event
+    @Input() eventsPerDay: [string, Event[]][] = [];
+
+    /** Store the selected Event */
+    @Input() selectedEvent: Event | null = null;
+
+    /** Whether or not to disable the links on the page */
+    @Input() disableLinks: boolean = false;
+
+    @Output() onCardClicked: EventEmitter<Event> = new EventEmitter()
 
     /** Constructs the widget */
     constructor() { }
