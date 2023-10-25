0febc9aacb7555611fffff3437cab9107343ee50
Kris Jordan <krisjordan@gmail.com>
2023-10-24 20:17:08 -0400

    Add type for EventJson

--- frontend/src/app/event/event.model.ts
+++ frontend/src/app/event/event.model.ts
@@ -1,7 +1,7 @@
 /**
  * The Event Model defines the shape of Event data retrieved from
  * the Event Service and the API.
- * 
+ *
  * @author Ajay Gandecha, Jade Keegan, Brianna Ta, Audrey Toney
  * @copyright 2023
  * @license MIT
@@ -11,14 +11,14 @@ import { Organization } from '../organization/organization.model';
 
 /** Interface for Event Type (used on frontend for event detail) */
 export interface Event {
-    id: number | null;
-    name: string;
-    time: Date;
-    location: string;
-    description: string;
-    public: boolean;
-    organization_id: number | null;
-    organization: Organization | null;
+  id: number | null;
+  name: string;
+  time: Date;
+  location: string;
+  description: string;
+  public: boolean;
+  organization_id: number | null;
+  organization: Organization | null;
 }
 
 /** Interface for the Event JSON Response model
@@ -27,14 +27,14 @@ export interface Event {
  *  the job of the `parseEventJson` function to convert it to the `Event` type
  */
 export interface EventJson {
-    id: number | null;
-    name: string;
-    time: string;
-    location: string;
-    description: string;
-    public: boolean;
-    organization_id: number | null;
-    organization: Organization | null;
+  id: number | null;
+  name: string;
+  time: string;
+  location: string;
+  description: string;
+  public: boolean;
+  organization_id: number | null;
+  organization: Organization | null;
 }
 
 /** Function that converts an EventJSON response model to an Event model.
@@ -43,5 +43,5 @@ export interface EventJson {
  *  TypeScript objects ourselves.
  */
 export const parseEventJson = (eventJson: EventJson): Event => {
-    return Object.assign({}, eventJson, { time: new Date(eventJson.time) })
-}
+  return Object.assign({}, eventJson, { time: new Date(eventJson.time) });
+};
