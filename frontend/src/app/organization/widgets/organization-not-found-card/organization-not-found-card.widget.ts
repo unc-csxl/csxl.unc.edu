66dc28e98cde99deaa1fa4c2ed7151d4bb657239
Ajay Gandecha <ajgandecha@gmail.com>
2023-11-09 09:17:00 -0500

    Improve UX When Navigating to an Invalid Organization (#151)
    * Handle Error Handling from the Resolver to the Component for No Org Found
    * Implement Organization Not Found Widget

---
+++ frontend/src/app/organization/widgets/organization-not-found-card/organization-not-found-card.widget.ts
@@ -0,0 +1,19 @@
+/**
+ * The Organization Not Found Card widget abstracts the implementation
+ * of the missing organization card.
+ *
+ * @author Ajay Gandecha, Jade Keegan, Brianna Ta, Audrey Toney
+ * @copyright 2023
+ * @license MIT
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'organization-not-found-card',
+  templateUrl: './organization-not-found-card.widget.html',
+  styleUrls: ['./organization-not-found-card.widget.css']
+})
+export class OrganizationNotFoundCard {
+  constructor() {}
+}
