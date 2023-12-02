66c938e702ffdbaf1fb973ddf7397262d2311612
Ajay Gandecha <ajgandecha@gmail.com>
2023-12-01 22:29:08 -0500

    Update section_entity.py

--- backend/entities/courses/section_entity.py
+++ backend/entities/courses/section_entity.py
@@ -22,10 +22,6 @@ class SectionEntity(EntityBase):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     # Code of the section (for example, COMP 100-003's code would be "003")
     code: Mapped[str] = mapped_column(String, default="")
-    # Starting date for the term
-    start_date: Mapped[datetime] = mapped_column(DateTime)
-    # Ending date for the term
-    end_date: Mapped[datetime] = mapped_column(DateTime)
 
     # Course the section is for
     # NOTE: This defines a one-to-many relationship between the course and sections tables.
