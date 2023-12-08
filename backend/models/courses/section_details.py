2132fa4d3b4ce669651bd6264bb97bee6e426335
Ajay Gandecha <ajgandecha@gmail.com>
2023-12-08 14:59:07 -0500

    Add Detail Models with Relationships

---
+++ backend/models/courses/section_details.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from .course import Course
+from .term import Term
+
+__authors__ = ["Ajay Gandecha"]
+__copyright__ = "Copyright 2023"
+__license__ = "MIT"
+
+
+class SectionDetails(Section):
+    """
+    Pydantic model to represent an `Section`, including back-populated
+    relationship fields.
+
+    This model is based on the `SectionEntity` model, which defines the shape
+    of the `Section` database in the PostgreSQL database.
+    """
+
+    course: Course
+    term: Term
---
+++ backend/models/courses/term_details.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from .section import Section
+
+__authors__ = ["Ajay Gandecha"]
+__copyright__ = "Copyright 2023"
+__license__ = "MIT"
+
+
+class TermDetails(Term):
+    """
+    Pydantic model to represent an `Term`, including back-populated
+    relationship fields.
+
+    This model is based on the `TermEntity` model, which defines the shape
+    of the `Term` database in the PostgreSQL database.
+    """
+
+    course_sections: list[Section]
