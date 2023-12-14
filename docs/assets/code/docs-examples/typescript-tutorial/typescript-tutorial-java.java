7df18c16eeb117b6cba23f78267b7b6e374e971c
Ajay Gandecha <ajgandecha@gmail.com>
2023-12-14 16:08:44 -0500

    Add Sample Code for Example

---
+++ docs/assets/code/docs-examples/typescript-tutorial/typescript-tutorial-java.java
@@ -0,0 +1,8 @@
+
+// Examples for Variable and Constant Declarations
+
+// Declaring a number
+int myNumber = 88;
+
+// General formula
+type name = value;
---
+++ docs/assets/code/docs-examples/typescript-tutorial/typescript-tutorial-ts.ts
@@ -0,0 +1,8 @@
+
+// Examples for Variable and Constant Declarations
+
+// Declaring a number
+let myNumber: number = 88;
+
+// General formula
+let name: type = value;
