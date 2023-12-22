351a76e0c449f76aefc80b380800b9c021717520
Ajay Gandecha <ajgandecha@gmail.com>
2023-12-22 00:38:00 -0500

    Implement Academics Home Page

--- frontend/src/app/academics/academics-routing.module.ts
+++ frontend/src/app/academics/academics-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesHomeComponent } from './course-catalog/course-catalog.component';
 import { SectionOfferingsComponent } from './section-offerings/section-offerings.component';
+import { AcademicsHomeComponent } from './academics-home/academics-home.component';
 
 const routes: Routes = [
+  AcademicsHomeComponent.Route,
   CoursesHomeComponent.Route,
   SectionOfferingsComponent.Route
 ];
