d8e3de694ec573ff61863787d48854953478b753
Ajay Gandecha <ajgandecha@gmail.com>
2023-12-23 21:50:08 -0500

    Add showAdminSettingsNavigation to NavigationService

--- frontend/src/app/navigation/navigation.service.ts
+++ frontend/src/app/navigation/navigation.service.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { PermissionService } from '../permission.service';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,11 @@ export class NavigationService {
   >(null);
   public error$ = this._error.asObservable();
 
-  constructor() {}
+  private adminSettingsData: ReplaySubject<AdminSettingsNavigationData | null> =
+    new ReplaySubject(1);
+  public adminSettingsData$ = this.adminSettingsData.asObservable();
+
+  constructor(private permissionService: PermissionService) {}
 
   setTitle(title: string) {
     this._deferToNextChangeDetectionCycle(() => this.title.next(title));
@@ -43,6 +48,39 @@ export class NavigationService {
   }
 
   /**
+   * This function updates an internal reactive object setup to manage when to show the admin
+   * page gear icon or not.
+   *
+   * @param permissionAction Permission action that must pass for the admin settings to appear.
+   * @param permissionResource Permission resource that must pass for the admin settings to appear.
+   * @param tooltip Tooltip to display when hovering over the settings gear icon.
+   * @param targetUrl URL for the admin page for the button to redirect to.
+   */
+  public showAdminSettingsNavigation(
+    permissionAction: string,
+    permissionResource: string,
+    tooltip: string,
+    targetUrl: string
+  ) {
+    // First, check to see if the user has the permissions.
+    this.permissionService
+      .check(permissionAction, permissionResource)
+      .subscribe((hasPermission) => {
+        // If the user has the permission, then update the settings
+        // navigation data so that it shows. If not, clear the data.
+        if (hasPermission) {
+          // Update the settings data
+          this.adminSettingsData.next({
+            tooltip: tooltip,
+            url: targetUrl
+          });
+        } else {
+          // Reset the settings data
+          this.adminSettingsData.next(null);
+        }
+      });
+  }
+  /**
    * For reasons that seem related to HttpRequestInterceptor's lifecycle of asynchronous operations
    * being outside the general lifecycle of change detection in angular components, the following
    * workaround method is used to defer updating navigation state until the next tick and, therefore,
@@ -56,4 +94,9 @@ export class NavigationService {
   private _deferToNextChangeDetectionCycle(operation: () => void) {
     setTimeout(operation, 0);
   }
+}
+
+export interface AdminSettingsNavigationData {
+  tooltip: string;
+  url: string;
 }
