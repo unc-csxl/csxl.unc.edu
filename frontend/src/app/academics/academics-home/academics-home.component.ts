c24930541dc05a8f159f49fe6497f04bf8bff55c
Ajay Gandecha <ajgandecha@gmail.com>
2023-12-23 22:35:29 -0500

    Create Working Gear Prototype

--- frontend/src/app/academics/academics-home/academics-home.component.ts
+++ frontend/src/app/academics/academics-home/academics-home.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NagivationAdminGearService } from 'src/app/navigation/navigation-admin-gear.service';
+import { PermissionService } from 'src/app/permission.service';
 
 @Component({
   selector: 'app-academics-home',
   templateUrl: './academics-home.component.html',
   styleUrls: ['./academics-home.component.css']
 })
-export class AcademicsHomeComponent {
+export class AcademicsHomeComponent implements OnInit {
   /** Route information to be used in Course Routing Module */
   public static Route = {
     path: '',
@@ -14,4 +16,10 @@ export class AcademicsHomeComponent {
     component: AcademicsHomeComponent,
     canActivate: []
   };
+
+  constructor(private gearService: NagivationAdminGearService) {}
+
+  ngOnInit() {
+    this.gearService.showAdminGear('academics.*', '*', '', 'admin/');
+  }
 }
--- frontend/src/app/permission.service.ts
+++ frontend/src/app/permission.service.ts
@@ -9,10 +9,6 @@ import { AdminSettingsNavigationData } from './navigation/navigation.service';
 export class PermissionService {
   private profile$: Observable<Profile | undefined>;
 
-  private adminSettingsData: ReplaySubject<AdminSettingsNavigationData | null> =
-    new ReplaySubject(1);
-  public adminSettingsData$ = this.adminSettingsData.asObservable();
-
   constructor(profileService: ProfileService) {
     this.profile$ = profileService.profile$;
   }
@@ -56,39 +52,5 @@ export class PermissionService {
 
   private expandPattern(pattern: string): RegExp {
     return new RegExp(`^${pattern.replaceAll('*', '.*')}$`);
-  }
-
-  /**
-   * This function updates an internal reactive object setup to manage when to show the admin
-   * page gear icon or not.
-   *
-   * @param permissionAction Permission action that must pass for the admin settings to appear.
-   * @param permissionResource Permission resource that must pass for the admin settings to appear.
-   * @param tooltip Tooltip to display when hovering over the settings gear icon.
-   * @param targetUrl URL for the admin page for the button to redirect to.
-   */
-  public showAdminSettingsNavigation(
-    permissionAction: string,
-    permissionResource: string,
-    tooltip: string,
-    targetUrl: string
-  ) {
-    // First, check to see if the user has the permissions.
-    this.check(permissionAction, permissionResource).subscribe(
-      (hasPermission) => {
-        // If the user has the permission, then update the settings
-        // navigation data so that it shows. If not, clear the data.
-        if (hasPermission) {
-          // Update the settings data
-          this.adminSettingsData.next({
-            tooltip: tooltip,
-            url: targetUrl
-          });
-        } else {
-          // Reset the settings data
-          this.adminSettingsData.next(null);
-        }
-      }
-    );
   }
 }
