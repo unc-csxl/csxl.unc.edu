570ad98407e74dab7444f8df29cc2bbaefdd91f8
Matt Vu <tmattvu@ad.unc.edu>
2024-01-26 01:44:20 -0500

    Added text to notify user if they have or have not accepted; Accept button only pops up if user has not accepted

--- frontend/src/app/shared/community-agreement/community-agreement.widget.ts
+++ frontend/src/app/shared/community-agreement/community-agreement.widget.ts
@@ -1,20 +1,40 @@
-import { Component, Inject } from '@angular/core';
+/**
+ * The Community Agreement Widget showcases the agreement that existing and new users
+ * will have to sign. It is currently used on the coworking home page and the profile
+ * page. It also checks whether or not users have accepted.
+ *
+ * @author Matt Vu
+ * @copyright 2023
+ * @license MIT
+ */
+
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
-import { ProfileService } from 'src/app/profile/profile.service';
+import { Profile, ProfileService } from 'src/app/profile/profile.service';
 
 @Component({
   selector: 'community-agreement',
   templateUrl: './community-agreement.widget.html',
   styleUrls: ['./community-agreement.widget.css']
 })
-export class CommunityAgreement {
+export class CommunityAgreement implements OnInit {
+  public has_user_agreed: boolean = false;
+
   constructor(
     public dialogRef: MatDialogRef<CommunityAgreement>,
     public profileService: ProfileService,
     private http: HttpClient,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
+
+  ngOnInit(): void {
+    this.profileService.profile$.subscribe((profile) => {
+      if (profile) {
+        this.has_user_agreed = profile?.has_agreed;
+      }
+    });
+  }
 
   onCloseClick(): void {
     this.dialogRef.close();
