8c12298f1bac8f8498414908156a66c9440b77cf
Nick Wherthey <nwherthey@unc.edu>
2024-02-20 14:51:33 -0500

    Add Method to Define a Weeky Limit for Room Reservations
    In this commit, a method was added that can be called in order to check if a user has exceeded the weeky limit for reserving study rooms. Using the policy
    service is consistent with the rest of the codebase and allows the limit to easily
    be changed

--- backend/services/coworking/policy.py
+++ backend/services/coworking/policy.py
@@ -18,8 +18,7 @@ class PolicyService:
     for different groups of users (e.g. majors, ambassadors, LAs, etc).
     """
 
-    def __init__(self):
-        ...
+    def __init__(self): ...
 
     def walkin_window(self, _subject: User) -> timedelta:
         """How far into the future can walkins be reserved?"""
@@ -54,3 +53,7 @@ class PolicyService:
 
     def reservation_checkin_timeout(self) -> timedelta:
         return timedelta(minutes=10)
+
+    def room_reservation_weekly_limit(self) -> timedelta:
+        """The maximum amount of hours a student can reserve the study rooms outside of the csxl."""
+        return timedelta(hours=6)
