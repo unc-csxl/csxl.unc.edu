04c40e9e050609d6e477b47d9d6e050cccc80686
Kris Jordan <kris@cs.unc.edu>
2024-02-26 21:50:04 -0500

    Hotfixes to restore build success

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -40,7 +40,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { GateComponent } from './gate/gate.component';
 import { ProfileEditorComponent } from './profile/profile-editor/profile-editor.component';
-import { EventFilterPipe } from './event/event-filter/event-filter.pipe';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
