cc7c2ce0efefcda4f3aa58e79f81a2ae5f825b3b
John Schachte <johnpete@ad.unc.edu>
2024-03-01 17:31:53 -0500

    Add RxUpcomingReservation
    This fixes the flickering that was occuring.
    I am not totally confident in explaing it but this setup was copied from Kris' code.

--- frontend/src/app/coworking/rx-coworking-status.ts
+++ frontend/src/app/coworking/rx-coworking-status.ts
@@ -2,3 +2,5 @@ import { RxObject } from '../rx-object';
 import { CoworkingStatus, Reservation } from './coworking.models';
 
 export class RxCoworkingStatus extends RxObject<CoworkingStatus> {}
+
+export class RxUpcomingReservation extends RxObject<Reservation[]> {}
