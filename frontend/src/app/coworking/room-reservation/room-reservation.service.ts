3612f93d8a56f2cca708830190f4f3615825b71b
John Schachte <johnpete@ad.unc.edu>
2024-03-01 17:34:33 -0500

    Add pollUpcomingRoomReservations
    This will be called when the coworking home component needs to refresh every 10000ms.
    
    It takes in the Coworking home snackbar to alert users to errors.

--- frontend/src/app/coworking/room-reservation/room-reservation.service.ts
+++ frontend/src/app/coworking/room-reservation/room-reservation.service.ts
@@ -9,18 +9,28 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import {
   parseReservationJSON,
   Reservation,
   ReservationJSON
 } from '../coworking.models';
 import { ReservationService } from '../reservation/reservation.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RxUpcomingReservation } from '../rx-coworking-status';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomReservationService extends ReservationService {
+  private upcomingReservations: RxUpcomingReservation = new RxUpcomingReservation();
+  public upcomingReservations$: Observable<Reservation[]> = this.upcomingReservations.value$
+  public findActiveReservationPredicate: (r: Reservation) => boolean = (r:Reservation) => {
+    let now = new Date();
+    const activeStates = ["CONFIRMED","CHECKED_IN"];
+    return r.start <= now && r.end > now && activeStates.includes(r.state)
+  }
+  
   constructor(http: HttpClient) {
     super(http);
   }
@@ -51,5 +61,28 @@ export class RoomReservationService extends ReservationService {
     return this.http.delete<Reservation>(
       `/api/coworking/reservation/${reservation.id}`
     );
+  }
+
+  // private findActiveReservationPredicate(reservation:Reservation){
+  //   let now = new Date();
+  //   const activeStates = ["CONFIRMED","CHECKED_IN"];
+  //   return reservation.start <= now && reservation.end > now && activeStates.includes(reservation.state)
+  // }
+
+  pollUpcomingRoomReservation(snackBar: MatSnackBar){
+    console.log("running poll UpcomingRoomReservations");
+
+    // predicate to determine if this is a non active upcoming room reservation
+    const isUpcomingRoomReservation = (r:Reservation) => !this.findActiveReservationPredicate(r) && !!r && !!r.room
+    
+    this.getReservationsByState('CONFIRMED').pipe(
+      map(reservations => reservations.filter(r => isUpcomingRoomReservation(r))),
+      catchError((err: Error) => {
+        const message = 'Error while fetching upcoming reservations.';
+        snackBar.open(message, '', { duration: 8000 });
+        console.error(err);
+        return of([]);
+      })
+    ).subscribe((upcomingRoomReservations) => this.upcomingReservations.set(upcomingRoomReservations));
   }
 }
