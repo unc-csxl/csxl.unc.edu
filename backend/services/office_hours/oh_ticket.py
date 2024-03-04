9cee495a7b2902225800320494bac0a77a2fb89f
Madelyn Andrews <maddya30@email.unc.edu>
2024-03-04 17:08:54 -0500

    Add functionality to Office Hours ticket service

--- backend/services/office_hours/oh_ticket.py
+++ backend/services/office_hours/oh_ticket.py
@@ -1,12 +1,15 @@
 from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+
+from ..exceptions import ResourceNotFoundException
 from ...database import db_session
 from ...entities.office_hours.oh_ticket_entity import OfficeHoursTicketEntity
 from ...models.office_hours.oh_ticket import (
     OfficeHoursTicket,
     OfficeHoursTicketDraft,
 )
+from ...models.office_hours.ticket_state import TicketState
 from ...models.office_hours.oh_ticket_details import OfficeHoursTicketDetails
 from ...models.user import User
 
@@ -40,8 +43,16 @@ class OfficeHoursTicketService:
         Returns:
             OfficeHoursTicketDetails: Object added to table
         """
-        # TODO
-        return None
+        # TODO: add permissions
+        # Create new object
+        oh_ticket_entity = OfficeHoursTicketEntity.from_model(oh_ticket)
+
+        # Add new object to table and commit changes
+        self._session.add(oh_ticket_entity)
+        self._session.commit()
+
+        # Return added object
+        return oh_ticket_entity.to_details_model()
 
     def get_ticket_by_id(
         self, subject: User, oh_ticket_id: int
@@ -53,8 +64,20 @@ class OfficeHoursTicketService:
         Returns:
             OfficeHoursTicketDetails: `OfficeHoursTicketDetails` with the given id
         """
-        # TODO
-        return None
+
+        # Select all entries in the `OfficeHoursTicket` table and filter by id
+        query = select(OfficeHoursTicketEntity).filter(
+            OfficeHoursTicketEntity.id == oh_ticket_id
+        )
+        oh_ticket_entity = self._session.scalars(query).one_or_none()
+
+        # Raise an error if no entity was found.
+        if oh_ticket_entity is None:
+            raise ResourceNotFoundException(
+                f"Ticket with id: {oh_ticket_id} does not exist."
+            )
+
+        return oh_ticket_entity.to_details_model()
 
     def update(
         self, subject: User, oh_ticket: OfficeHoursTicket
@@ -66,18 +89,70 @@ class OfficeHoursTicketService:
         Returns:
             OfficeHoursTicketDetails: Updated object in table
         """
-        # TODO
-        return None
+        # TODO: permissions
 
-    def update_state(
-        self, subject: User, oh_ticket: OfficeHoursTicket
-    ) -> OfficeHoursTicketDetails:
-        """Updates an office hours ticket's state.
+        # Find the entity to update
+        oh_ticket_entity = self._session.get(OfficeHoursTicketEntity, oh_ticket.id)
+
+        # Raise an error if no entity was found
+        if oh_ticket_entity is None:
+            raise ResourceNotFoundException(
+                f"Course with id: {oh_ticket.id} does not exist."
+            )
+
+        # Update the entity
+        oh_ticket_entity.description = oh_ticket.description
+        oh_ticket_entity.type = oh_ticket.type
+
+        # Update state if its a valid transition
+        if self._valid_state_transition(oh_ticket_entity.state, oh_ticket.state):
+            oh_ticket_entity.state = oh_ticket.state
+
+            # NOTE: are these conditionals too complicated/unnecessary?
+
+            # Update the caller ID and time if ticket is now called
+            if oh_ticket_entity.state == TicketState.CALLED:
+                oh_ticket_entity.caller_id = oh_ticket.caller_id
+                # TODO: update call time once added to model
+                # oh_ticket_entity.called_at = oh_ticket.called_at
+
+            # Update closed time + TA feedback if ticket is now closed
+            if oh_ticket_entity.state == TicketState.CLOSED:
+                oh_ticket_entity.closed_at = oh_ticket.closed_at
+                oh_ticket_entity.have_concerns = oh_ticket.have_concerns
+                oh_ticket_entity.caller_notes = oh_ticket.caller_notes
+
+        # Commit changes
+        self._session.commit()
+
+        # Return edited object
+        return oh_ticket_entity.to_details_model()
+
+    def _valid_state_transition(
+        self, prior_state: TicketState, new_state: TicketState
+    ) -> bool:
+        """Checks if a state transition is valid for an OfficeHoursTicket.
         Args:
-            subject: a valid User model representing the currently logged in User
-            oh_ticket: OfficeHoursTicket to update in the table
+            prior_state: previous state of the ticket entity
+            new_state: state that the ticket needs to be updated to
         Returns:
-            OfficeHoursTicketDetails: Updated object in table
+            valid_transition: boolean stating whether state transition is allowed
         """
-        # TODO
-        return None
+        TS = TicketState
+
+        transition = (prior_state, new_state)
+        valid_transition = False
+
+        match transition:
+            case (TS.PENDING, TS.CALLED):
+                valid_transition = True
+            case (TS.PENDING, TS.CANCELED):
+                valid_transition = True
+            case (TS.CALLED, TS.CLOSED):
+                valid_transition = True
+            case (TS.CALLED, TS.CANCELED):
+                valid_transition = True
+            case _:
+                return False
+
+        return valid_transition
