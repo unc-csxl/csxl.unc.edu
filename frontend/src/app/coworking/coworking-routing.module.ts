f6d31272415989491d525632f32551dd83a817c6
aarjav <ajain1@unc.edu>
2024-03-04 20:41:22 -0500

    Update imports in coworking routing module

--- frontend/src/app/coworking/coworking-routing.module.ts
+++ frontend/src/app/coworking/coworking-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoworkingPageComponent } from './coworking-home/coworking-home.component';
-import { AmbassadorPageComponent } from './ambassador-home/ambassador-home.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { NewReservationPageComponent } from './room-reservation/new-reservation-page/new-reservation-page.component';
 import { ConfirmReservationComponent } from './room-reservation/confirm-reservation/confirm-reservation.component';
-import { AmbassadorXlListComponent } from './ambassador-home/ambassador-xl/list/ambassador-xl-list.component';
 
 const routes: Routes = [
   CoworkingPageComponent.Route,
