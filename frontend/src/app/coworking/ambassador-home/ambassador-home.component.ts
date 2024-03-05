95e5ab36d4d71998ac9f26c563e2f2a45d675a43
aarjav <ajain1@unc.edu>
2024-03-04 20:39:39 -0500

    Modify new ambassador home component

--- frontend/src/app/coworking/ambassador-home/ambassador-home.component.ts
+++ frontend/src/app/coworking/ambassador-home/ambassador-home.component.ts
@@ -11,43 +11,11 @@ import { AmbassadorService } from './ambassador.service';
   templateUrl: './ambassador-home.component.html',
   styleUrls: ['./ambassador-home.component.css']
 })
-export class AmbassadorPageComponent implements OnInit, OnDestroy {
-  /** Route information to be used in App Routing Module */
-  public static Route: Route = {
-    path: 'ambassador',
-    component: AmbassadorPageComponent,
-    title: 'XL Ambassador',
-    canActivate: [permissionGuard('coworking.reservation.*', '*')],
-    resolve: { profile: profileResolver }
-  };
+export class AmbassadorPageComponent {
+  public links = [
+    { label: 'XL Reservations', path: '/coworking/ambassador/xl' },
+    { label: 'Room Reservations', path: '/coworking/ambassador/room' }
+  ];
 
-  reservations$: Observable<Reservation[]>;
-  upcomingReservations$: Observable<Reservation[]>;
-  activeReservations$: Observable<Reservation[]>;
-
-  columnsToDisplay = ['id', 'name', 'seat', 'start', 'end', 'actions'];
-
-  private refreshSubscription!: Subscription;
-
-  constructor(public ambassadorService: AmbassadorService) {
-    this.reservations$ = this.ambassadorService.reservations$;
-    this.upcomingReservations$ = this.reservations$.pipe(
-      map((reservations) => reservations.filter((r) => r.state === 'CONFIRMED'))
-    );
-    this.activeReservations$ = this.reservations$.pipe(
-      map((reservations) =>
-        reservations.filter((r) => r.state === 'CHECKED_IN')
-      )
-    );
-  }
-
-  ngOnInit(): void {
-    this.refreshSubscription = timer(0, 5000)
-      .pipe(tap((_) => this.ambassadorService.fetchReservations()))
-      .subscribe();
-  }
-
-  ngOnDestroy(): void {
-    this.refreshSubscription.unsubscribe();
-  }
+  constructor(public ambassadorService: AmbassadorService) {}
 }
