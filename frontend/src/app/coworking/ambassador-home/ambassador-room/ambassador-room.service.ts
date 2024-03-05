b29568fdc13eeccd39b541b2e372e26027ca27ed
aarjav <ajain1@unc.edu>
2024-03-04 20:41:23 -0500

    Test data for ambassador-room-list component

--- frontend/src/app/coworking/ambassador-home/ambassador-room/ambassador-room.service.ts
+++ frontend/src/app/coworking/ambassador-home/ambassador-room/ambassador-room.service.ts
@@ -1,9 +1,69 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  Reservation,
+  ReservationJSON,
+  parseReservationJSON
+} from '../../coworking.models';
+import { RxReservations } from '../rx-reservations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AmbassadorRoomService {
+  private reservations: RxReservations = new RxReservations();
+  public reservations$: Observable<Reservation[]> = this.reservations.value$;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  fetchReservations(): void {
+    this.http
+      .get<ReservationJSON[]>('/api/coworking/ambassador')
+      .subscribe((reservations) => {
+        this.reservations.set(reservations.map(parseReservationJSON));
+      });
+  }
+
+  checkIn(reservation: Reservation): void {
+    this.http
+      .put<ReservationJSON>(`/api/coworking/ambassador/checkin`, {
+        id: reservation.id,
+        state: 'CHECKED_IN'
+      })
+      .subscribe((reservationJson) => {
+        this.reservations.updateReservation(
+          parseReservationJSON(reservationJson)
+        );
+      });
+  }
+
+  checkOut(reservation: Reservation) {
+    this.http
+      .put<ReservationJSON>(`/api/coworking/reservation/${reservation.id}`, {
+        id: reservation.id,
+        state: 'CHECKED_OUT'
+      })
+      .subscribe((reservationJson) => {
+        this.reservations.updateReservation(
+          parseReservationJSON(reservationJson)
+        );
+      });
+  }
+
+  cancel(reservation: Reservation) {
+    this.http
+      .put<ReservationJSON>(`/api/coworking/reservation/${reservation.id}`, {
+        id: reservation.id,
+        state: 'CANCELLED'
+      })
+      .subscribe({
+        next: (_) => {
+          this.reservations.remove(reservation);
+        },
+        error: (err) => {
+          alert(err);
+        }
+      });
+  }
 }
--- frontend/src/app/coworking/ambassador-home/ambassador-room/list/ambassador-room-list.component.ts
+++ frontend/src/app/coworking/ambassador-home/ambassador-room/list/ambassador-room-list.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Route } from '@angular/router';
+import { Observable, Subscription, map, timer, tap } from 'rxjs';
+import { Reservation } from 'src/app/coworking/coworking.models';
 import { permissionGuard } from 'src/app/permission.guard';
+import { AmbassadorRoomService } from '../ambassador-room.service';
 
 @Component({
   selector: 'app-ambassador-room-list',
   templateUrl: './ambassador-room-list.component.html',
   styleUrls: ['./ambassador-room-list.component.css']
 })
-export class AmbassadorRoomListComponent {
+export class AmbassadorRoomListComponent implements OnInit, OnDestroy {
   public static Route: Route = {
     path: 'room',
     component: AmbassadorRoomListComponent,
@@ -15,4 +18,34 @@ export class AmbassadorRoomListComponent {
     canActivate: [permissionGuard('coworking.reservation.*', '*')],
     resolve: {}
   };
+
+  reservations$: Observable<Reservation[]>;
+  upcomingReservations$: Observable<Reservation[]>;
+  activeReservations$: Observable<Reservation[]>;
+
+  columnsToDisplay = ['id', 'name', 'seat', 'start', 'end', 'actions'];
+
+  private refreshSubscription!: Subscription;
+
+  constructor(public ambassadorService: AmbassadorRoomService) {
+    this.reservations$ = this.ambassadorService.reservations$;
+    this.upcomingReservations$ = this.reservations$.pipe(
+      map((reservations) => reservations.filter((r) => r.state === 'CONFIRMED'))
+    );
+    this.activeReservations$ = this.reservations$.pipe(
+      map((reservations) =>
+        reservations.filter((r) => r.state === 'CHECKED_IN')
+      )
+    );
+  }
+
+  ngOnInit(): void {
+    this.refreshSubscription = timer(0, 5000)
+      .pipe(tap((_) => this.ambassadorService.fetchReservations()))
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.refreshSubscription.unsubscribe();
+  }
 }
