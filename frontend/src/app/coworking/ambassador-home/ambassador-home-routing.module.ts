ddaa156b4b13b9c42b4e0ffc5a76b352e64972d1
aarjav <ajain1@unc.edu>
2024-03-04 20:41:23 -0500

    Update children in ambassador home routing module

--- frontend/src/app/coworking/ambassador-home/ambassador-home-routing.module.ts
+++ frontend/src/app/coworking/ambassador-home/ambassador-home-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AmbassadorXlListComponent } from './ambassador-xl/list/ambassador-xl-list.component';
 import { AmbassadorPageComponent } from './ambassador-home.component';
+import { AmbassadorRoomListComponent } from './ambassador-room/list/ambassador-room-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AmbassadorPageComponent,
-    children: [AmbassadorXlListComponent.Route]
+    children: [
+      AmbassadorXlListComponent.Route,
+      AmbassadorRoomListComponent.Route
+    ]
   }
 ];
 
