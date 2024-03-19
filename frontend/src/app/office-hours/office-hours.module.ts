3e8e267d1f795bf9759238e47ea7c8d13c6f447a
Madelyn Andrews <maddya30@email.unc.edu>
2024-03-18 23:25:10 -0400

    Started work on the frontend for student view of an office hours section:
    - Added mat tab group to office hours section home page
    - Created event card widget with hard-coded information [WIP]
    - Created schedule card widget with hard-coded information [WIP]

--- frontend/src/app/office-hours/office-hours.module.ts
+++ frontend/src/app/office-hours/office-hours.module.ts
@@ -3,9 +3,22 @@ import { CommonModule } from '@angular/common';
 import { OfficeHoursPageComponent } from './office-hours-page/office-hours-page.component';
 import { OfficeHoursRoutingModule } from './office-hours-routing.module';
 import { StudentSectionHomeComponent } from './student-section-home/student-section-home.component';
+import { EventCard } from './widgets/event-card/event-card-widget';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ScheduleCard } from './widgets/schedule-card/schedule-card-widget';
+import { MatCardModule } from '@angular/material/card';
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  declarations: [OfficeHoursPageComponent, StudentSectionHomeComponent],
-  imports: [CommonModule, OfficeHoursRoutingModule]
+  declarations: [OfficeHoursPageComponent, StudentSectionHomeComponent, EventCard, ScheduleCard],
+  imports: [
+    CommonModule,
+    OfficeHoursRoutingModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatDividerModule
+  ]
 })
 export class OfficeHoursModule {}
---
+++ frontend/src/app/office-hours/widgets/event-card/event-card-widget.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-card-widget',
+  templateUrl: './event-card-widget.html',
+  styleUrls: ['./event-card-widget.css']
+})
+
+export class EventCard {
+
+}
