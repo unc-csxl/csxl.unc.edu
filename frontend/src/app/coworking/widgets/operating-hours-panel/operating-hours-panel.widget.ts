d9f01e8d32d851287c036676c336b40180f2dc03
Ajay Gandecha <ajgandecha@gmail.com>
2024-04-01 09:32:43 -0400

    Add Reopening Time to OH Panel, Make OH Panel Responsive

--- frontend/src/app/coworking/widgets/operating-hours-panel/operating-hours-panel.widget.ts
+++ frontend/src/app/coworking/widgets/operating-hours-panel/operating-hours-panel.widget.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { OperatingHours } from 'src/app/coworking/coworking.models';
 import { OperatingHoursDialog } from '../operating-hours-dialog/operating-hours-dialog.widget';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'coworking-operating-hours-panel',
@@ -18,5 +19,16 @@ export class CoworkingHoursCard {
     const dialogRef = this.dialog.open(OperatingHoursDialog, {
       data: this.operatingHours
     });
+  }
+}
+
+/** Local pipe that capitalizes the first letter of the string. */
+@Pipe({
+  name: 'firstLetterCapital'
+})
+export class FirstLetterCapitalPipe implements PipeTransform {
+  transform(sentence: string | null | undefined): string {
+    if (!sentence) return '';
+    return sentence[0].toUpperCase() + sentence.substring(1).toLowerCase();
   }
 }
