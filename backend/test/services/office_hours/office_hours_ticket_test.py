62c6b1fb1cd2cd5ca4694b77868f9b3c59c72d81
Meghan Sun <meghansun322@gmail.com>
2024-04-02 11:54:19 -0400

    Initial OH Ticket Service Tests

---
+++ backend/test/services/office_hours/office_hours_ticket_test.py
@@ -0,0 +1,42 @@
+"""Tests for Courses Section Service."""
+
+from unittest.mock import create_autospec
+import pytest
+from backend.models.office_hours.ticket_details import OfficeHoursTicketDetails
+from backend.services.exceptions import (
+    ResourceNotFoundException,
+    UserPermissionException,
+)
+from backend.services.office_hours.ticket import OfficeHoursTicketService
+from backend.services.permission import PermissionService
+
+
+# Imported fixtures provide dependencies injected for the tests as parameters.
+from .fixtures import permission_svc, oh_ticket_svc
+
+# Import the setup_teardown fixture explicitly to load entities in database
+from ..core_data import setup_insert_data_fixture as insert_order_0
+from ..room_data import fake_data_fixture as insert_order_1
+from ..academics.term_data import fake_data_fixture as insert_order_2
+from ..academics.course_data import fake_data_fixture as insert_order_3
+from ..academics.section_data import fake_data_fixture as insert_order_4
+from .office_hours_data import fake_data_fixture as insert_order_5
+
+# Import the fake model data in a namespace for test assertions
+from . import office_hours_data
+from .. import user_data
+from ..academics import section_data
+
+
+# TODO: Add Comments
+def test_create_ticket(oh_ticket_svc: OfficeHoursTicketService):
+    ticket = oh_ticket_svc.create(user_data.user, office_hours_data.ticket_draft)
+    assert isinstance(ticket, OfficeHoursTicketDetails)
+
+
+def test_create_ticket_exception_for_non_section_member(
+    oh_ticket_svc: OfficeHoursTicketService,
+):
+    with pytest.raises(PermissionError):
+        oh_ticket_svc.create(user_data.root, office_hours_data.ticket_draft)
+        pytest.fail()  # Fail test if no error was thrown above
