2ed2382e053e289126793f08c4b309b26b7247c5
Bailey DeSouza <bdesouza@email.unc.edu>
2024-04-04 21:00:19 -0400

    Moved upcoming schedule logic to schedule card widget

--- frontend/src/app/office-hours/widgets/schedule-card/schedule-card-widget.ts
+++ frontend/src/app/office-hours/widgets/schedule-card/schedule-card-widget.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { OfficeHoursService } from '../../office-hours.service';
+import {
+  OfficeHoursEvent,
+  OfficeHoursEventType
+} from '../../office-hours.models';
 
 @Component({
   selector: 'schedule-card-widget',
   templateUrl: './schedule-card-widget.html',
   styleUrls: ['./schedule-card-widget.css']
 })
-export class ScheduleCard {}
+export class ScheduleCard implements OnInit {
+  @Input() sectionId!: number;
+  upcomingHours: OfficeHoursEvent[] = [];
+
+  constructor(private officeHoursService: OfficeHoursService) {}
+
+  ngOnInit(): void {
+    this.getUpcomingHours();
+  }
+
+  getUpcomingHours() {
+    this.officeHoursService
+      .getUpcomingEventsBySection(this.sectionId)
+      .subscribe((hours) => {
+        this.upcomingHours = hours;
+      });
+  }
+
+  formatEventType(typeNum: number) {
+    if (typeNum === OfficeHoursEventType.OFFICE_HOURS) {
+      return 'Office Hours';
+    } else if (typeNum === OfficeHoursEventType.TUTORING) {
+      return 'Tutoring';
+    } else if (typeNum === OfficeHoursEventType.REVIEW_SESSION) {
+      return 'Review Session';
+    } else if (typeNum === OfficeHoursEventType.VIRTUAL_OFFICE_HOURS) {
+      return 'Virtual Office Hours';
+    } else if (typeNum === OfficeHoursEventType.VIRTUAL_TUTORING) {
+      return 'Virtual Tutoring';
+    } else if (typeNum === OfficeHoursEventType.VIRTUAL_REVIEW_SESSION) {
+      return 'Virtual Review Session';
+    } else {
+      return 'error';
+    }
+  }
+}
