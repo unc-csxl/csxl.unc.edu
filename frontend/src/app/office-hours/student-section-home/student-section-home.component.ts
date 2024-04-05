08125e5b17aee52649ef512d0c76a8c07e76596a
Bailey DeSouza <bdesouza@email.unc.edu>
2024-04-04 23:47:02 -0400

    Added hover over title in course card - unhard coded event card type, title, and end time

--- frontend/src/app/office-hours/student-section-home/student-section-home.component.ts
+++ frontend/src/app/office-hours/student-section-home/student-section-home.component.ts
@@ -9,13 +9,15 @@
 
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, RouteConfigLoadEnd } from '@angular/router';
+import { OfficeHoursEvent } from '../office-hours.models';
+import { OfficeHoursService } from '../office-hours.service';
 
 @Component({
   selector: 'app-student-section-home',
   templateUrl: './student-section-home.component.html',
   styleUrls: ['./student-section-home.component.css']
 })
-export class StudentSectionHomeComponent {
+export class StudentSectionHomeComponent implements OnInit {
   public static Route = {
     // placeholder route
     path: 'spring-2024/:id',
@@ -23,14 +25,30 @@ export class StudentSectionHomeComponent {
     component: StudentSectionHomeComponent,
     canActivate: []
   };
+  currentEvents: OfficeHoursEvent[] = [];
   sectionId: number;
   navLinks: any;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private officeHoursService: OfficeHoursService
+  ) {
     let navLinks = [
       { path: '/events', label: 'Events' },
       { path: '/history', label: 'History' }
     ];
     this.sectionId = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.getCurrentEvents();
+  }
+
+  getCurrentEvents() {
+    this.officeHoursService
+      .getCurrentEventsBySection(this.sectionId)
+      .subscribe((events) => {
+        this.currentEvents = events;
+      });
   }
 }
