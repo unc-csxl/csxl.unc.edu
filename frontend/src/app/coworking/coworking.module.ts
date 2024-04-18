e3c8b9717272de14dee82234612e7e6ccb425b50
aarjav <ajain1@unc.edu>
2024-04-18 14:17:53 -0400

    Update declarations in coworking module to add RoomCapacityDialogComponent

--- frontend/src/app/coworking/coworking.module.ts
+++ frontend/src/app/coworking/coworking.module.ts
@@ -31,6 +31,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { AmbassadorXlListComponent } from './ambassador-home/ambassador-xl/list/ambassador-xl-list.component';
 import { AmbassadorRoomListComponent } from './ambassador-home/ambassador-room/list/ambassador-room-list.component';
+import { RoomCapacityDialogComponent } from './widgets/room-dialogue/room-dialogue.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { AmbassadorRoomListComponent } from './ambassador-home/ambassador-room/l
     ConfirmReservationComponent,
     NewReservationPageComponent,
     DateSelector,
-    OperatingHoursDialog
+    OperatingHoursDialog,
+    RoomCapacityDialogComponent
   ],
   imports: [
     CommonModule,
