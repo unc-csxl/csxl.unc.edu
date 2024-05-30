ddbb216bf789329012e37e0db5ea5294844ffe9f
Ajay Gandecha <ajgandecha@gmail.com>
2024-05-30 15:25:46 -0400

    Complete Sidebar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   template: '<app-navigation></app-navigation>'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private matIconReg: MatIconRegistry) {}
+
+  ngOnInit() {
+    this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
+  }
 }
