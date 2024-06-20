a46869ec4f38362d789e1050e6d8f3ad86101fb5
Ajay Gandecha <ajgandecha@gmail.com>
2024-06-19 23:38:43 -0400

    Remove Unused Section Member API

--- backend/api/academics/section_member.py
+++ backend/api/academics/section_member.py
@@ -49,34 +49,6 @@ def get_section_member_by_id(
         raise HTTPException(status_code=404, detail=str(e))
 
 
-@api.get("/oh-section/{section_id}", response_model=SectionMember, tags=["Academics"])
-def get_membership_by_user_and_oh_section_id(
-    section_id: int,
-    subject: User = Depends(registered_user),
-    section_member_svc: SectionMemberService = Depends(),
-) -> SectionMember:
-    """
-    Retrieves a SectionMember's membership in an Office Hours section by section ID.
-
-    Args:
-        section_id (int): The ID of the Office Hours section.
-        subject (User): The currently logged-in user.
-        section_member_svc (SectionMemberService): Service dependency to interact with Section Membership data.
-
-    Returns:
-        SectionMember: The SectionMember's membership in the specified Office Hours section.
-
-    Raises:
-        HTTPException(404): If the SectionMember's membership in the specified section is not found.
-    """
-    try:
-        return section_member_svc.get_section_member_by_user_id_and_oh_section_id(
-            subject, section_id
-        )
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
-
 @api.post("", response_model=list[SectionMember], tags=["Academics"])
 def add_user_memberships(
     oh_sections: list[OfficeHoursSection],
