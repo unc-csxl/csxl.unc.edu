c1a57cdb3a6e7ea73ed7fa605815c3e671b5076b
Ajay Gandecha <ajgandecha@gmail.com>
2024-07-20 14:13:01 -0400

    Fix Slack Invite Dialog Design

--- frontend/src/app/navigation/widgets/slack-invite-box/slack-invite-box.widget.ts
+++ frontend/src/app/navigation/widgets/slack-invite-box/slack-invite-box.widget.ts
@@ -3,12 +3,13 @@
  * CSXL Slack as a dialog box. It requires that students fulfill
  * four requirements represented as checkboxes.
  *
- * @author Rohan Kashyap, Kris Jordan
+ * @author Rohan Kashyap, Kris Jordan, Ajay Gandecha
  * @copyright 2024
  * @license MIT
  */
 
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'slack-invite-box',
@@ -17,25 +18,23 @@ import { Component } from '@angular/core';
 })
 export class SlackInviteBox {
   public realName: boolean = false;
-  public profilePicture: boolean = false;
   public communityStandards: boolean = false;
-  public fAroundFindOut: boolean = false;
 
-  public get acceptAll(): boolean {
-    return (
-      this.realName &&
-      this.profilePicture &&
-      this.communityStandards &&
-      this.fAroundFindOut
-    );
+  public get acceptedAll(): boolean {
+    return this.realName && this.communityStandards;
   }
 
-  constructor() {}
+  constructor(protected dialogRef: MatDialogRef<SlackInviteBox>) {}
 
   public openSlackInvite() {
     window.open(
       'https://join.slack.com/t/csxl/shared_invite/zt-1mvfaiqme-fXXw9cKjaXOfhXVBfgXVCA',
       '_blank'
     );
+  }
+
+  /** Closes the dialog */
+  close(): void {
+    this.dialogRef.close();
   }
 }
