b833892fa57153ba7cefc2780a9f40421b7e2741
Ajay Gandecha <ajgandecha@gmail.com>
2024-07-21 18:19:33 -0400

    Hide Settings Tab from Students and TAs

--- frontend/src/app/my-courses/course/course.component.ts
+++ frontend/src/app/my-courses/course/course.component.ts
@@ -6,8 +6,21 @@
  * @license MIT
  */
 
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  OnInit,
+  signal,
+  WritableSignal
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MyCoursesService } from '../my-courses.service';
+import {
+  parseTermOverviewJsonList,
+  TermOverviewJson
+} from '../my-courses.model';
+import { map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-course',
@@ -16,23 +29,59 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CourseComponent {
   /** Links for the tab bar */
-  public links = [
-    {
-      label: 'Office Hours',
-      path: `/course/${this.route.snapshot.params['course_site_id']}/office-hours`,
-      icon: 'person_raised_hand'
-    },
-    {
-      label: 'Roster',
-      path: `/course/${this.route.snapshot.params['course_site_id']}/roster`,
-      icon: 'groups'
-    },
-    {
-      label: 'Settings',
-      path: `/course/${this.route.snapshot.params['course_site_id']}/settings`,
-      icon: 'settings'
-    }
-  ];
+  isInstructor: WritableSignal<boolean> = signal(false);
 
-  constructor(private route: ActivatedRoute) {}
+  links = computed(() => {
+    if (this.isInstructor()) {
+      return [
+        {
+          label: 'Office Hours',
+          path: `/course/${this.route.snapshot.params['course_site_id']}/office-hours`,
+          icon: 'person_raised_hand'
+        },
+        {
+          label: 'Roster',
+          path: `/course/${this.route.snapshot.params['course_site_id']}/roster`,
+          icon: 'groups'
+        },
+        {
+          label: 'Settings',
+          path: `/course/${this.route.snapshot.params['course_site_id']}/settings`,
+          icon: 'settings'
+        }
+      ];
+    } else {
+      return [
+        {
+          label: 'Office Hours',
+          path: `/course/${this.route.snapshot.params['course_site_id']}/office-hours`,
+          icon: 'person_raised_hand'
+        },
+        {
+          label: 'Roster',
+          path: `/course/${this.route.snapshot.params['course_site_id']}/roster`,
+          icon: 'groups'
+        }
+      ];
+    }
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    protected myCoursesService: MyCoursesService,
+    protected http: HttpClient
+  ) {
+    let id = +this.route.snapshot.params['course_site_id'];
+    this.http
+      .get<TermOverviewJson[]>('/api/my-courses')
+      .pipe(map(parseTermOverviewJsonList))
+      .subscribe((terms) => {
+        console.log(terms);
+        let isInstructor =
+          terms.flatMap((term) => term.sites).find((site) => site.id === id)
+            ?.role == 'Instructor';
+
+        this.isInstructor.set(isInstructor);
+      });
+  }
 }
