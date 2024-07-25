26028c99fe28a5a68cf79c3f5f3678182892a74d
Ajay Gandecha <ajgandecha@gmail.com>
2024-07-24 23:01:26 -0400

    Create New Hiring APIs

--- backend/api/academics/hiring.py
+++ backend/api/academics/hiring.py
@@ -7,6 +7,8 @@ from fastapi import APIRouter, Depends
 from ...services.academics import HiringService
 
 from ...models.academics.hiring.application_review import HiringStatus
+from ...models.academics.hiring.hiring_assignment import *
+from ...models.academics.hiring.hiring_level import *
 
 from ...api.authentication import registered_user
 from ...models.user import User
@@ -46,3 +48,86 @@ def update_status(
     Updates the hiring status for TA Applications.
     """
     return hiring_service.update_status(subject, course_site_id, hiring_status)
+
+
+@api.get("/admin/{term_id}", tags=["Hiring"])
+def get_hiring_admin_overview(
+    term_id: str,
+    subject: User = Depends(registered_user),
+    hiring_service: HiringService = Depends(),
+) -> HiringAdminOverview:
+    """
+    Returns the state of hiring to the admin.
+    """
+    return hiring_service.get_hiring_admin_overview(subject, term_id)
+
+
+@api.post("/assignment", tags=["Hiring"])
+def create_hiring_assignment(
+    assignment: HiringAssignmentDraft,
+    subject: User = Depends(registered_user),
+    hiring_service: HiringService = Depends(),
+) -> HiringAssignmentOverview:
+    """
+    Creates a new assignment
+    """
+    return hiring_service.create_hiring_assignment(subject, assignment)
+
+
+@api.put("/assignment", tags=["Hiring"])
+def update_hiring_assignment(
+    assignment: HiringAssignmentDraft,
+    subject: User = Depends(registered_user),
+    hiring_service: HiringService = Depends(),
+) -> HiringAssignmentOverview:
+    """
+    Updates an assignment
+    """
+    return hiring_service.update_hiring_assignment(subject, assignment)
+
+
+@api.delete("/assignment/{assignment_id}", tags=["Hiring"])
+def delete_hiring_assignment(
+    assignment_id: int,
+    subject: User = Depends(registered_user),
+    hiring_service: HiringService = Depends(),
+) -> HiringAssignmentOverview:
+    """
+    Deletes an assignment
+    """
+    return hiring_service.delete_hiring_assignment(subject, assignment_id)
+
+
+@api.get("/level", tags=["Hiring"])
+def get_hiring_levels(
+    subject: User = Depends(registered_user),
+    hiring_service: HiringService = Depends(),
+) -> list[HiringLevel]:
+    """
+    Get all hiring levels
+    """
+    return hiring_service.get_hiring_levels(subject)
+
+
+@api.post("/level", tags=["Hiring"])
+def create_hiring_level(
+    level: HiringLevel,
+    subject: User = Depends(registered_user),
+    hiring_service: HiringService = Depends(),
+) -> HiringAssignmentOverview:
+    """
+    Creates a new level
+    """
+    return hiring_service.create_hiring_level(subject, level)
+
+
+@api.put("/level", tags=["Hiring"])
+def update_hiring_level(
+    level: HiringLevel,
+    subject: User = Depends(registered_user),
+    hiring_service: HiringService = Depends(),
+) -> HiringAssignmentOverview:
+    """
+    Updates a level
+    """
+    return hiring_service.update_hiring_level(subject, level)
