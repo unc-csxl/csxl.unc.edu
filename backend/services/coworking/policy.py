d274f4a1943fb00e945fdab4afa6b33b3fd1800b
Kris Jordan <krisjordan@gmail.com>
2024-09-16 10:39:10 -0400

    Update policy with reserved rooms

--- backend/services/coworking/policy.py
+++ backend/services/coworking/policy.py
@@ -46,7 +46,7 @@ OH_HOURS = {
         "SN139": [(time(hour=14), time(hour=15))],
         "SN141": [(time(hour=14), time(hour=17))],
         "SN144": [(time(hour=13), time(hour=17))],
-        "SN146": [(time(hour=15), time(hour=17))],
+        "SN146": [(time(hour=10), time(hour=12))],
         "SN147": [(time(hour=10), time(hour=1)), (time(hour=14), time(hour=16))],
     },
     THURSDAY: {
