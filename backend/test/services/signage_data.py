4dee5402f357eabbd572b0e3003db7b550b6384c
wzahrt <wzahrt@unc.edu>
2024-10-28 14:05:05 -0400

    Basic tests for signage finished
    q

--- backend/test/services/signage_data.py
+++ backend/test/services/signage_data.py
@@ -12,7 +12,7 @@ from .core_data import user_data
 
 from . import room_data
 
-__authors__ = ["Will Zahrt"]
+__authors__ = ["Will Zahrt", "Andrew Lockard"]
 
 
 now = datetime.now()
@@ -30,11 +30,86 @@ current_reservation = Reservation(
     seats=[],
 )
 
+checked_out_reservation_1 = Reservation(
+    id=9,
+    start=now - timedelta(hours=2),
+    end=now - timedelta(hours=1),
+    created_at=now - timedelta(hours=2),
+    updated_at=now - timedelta(hours=1),
+    walkin=False,
+    room=None,
+    state=ReservationState.CHECKED_OUT,
+    users=[user_data.ambassador],
+    seats=[],
+)
+
+checked_out_reservation_2 = Reservation(
+    id=10,
+    start=now - timedelta(hours=4),
+    end=now - timedelta(hours=3),
+    created_at=now - timedelta(hours=4),
+    updated_at=now - timedelta(hours=3),
+    walkin=False,
+    room=None,
+    state=ReservationState.CHECKED_OUT,
+    users=[user_data.ambassador],
+    seats=[],
+)
+
+checked_out_reservation_3 = Reservation(
+    id=11,
+    start=now - timedelta(hours=2),
+    end=now - timedelta(hours=1),
+    created_at=now - timedelta(hours=2),
+    updated_at=now - timedelta(hours=1),
+    walkin=False,
+    room=None,
+    state=ReservationState.CHECKED_OUT,
+    users=[user_data.root],
+    seats=[],
+)
+
+checked_out_reservation_4 = Reservation(
+    id=12,
+    start=now - timedelta(hours=4),
+    end=now - timedelta(hours=3),
+    created_at=now - timedelta(hours=4),
+    updated_at=now - timedelta(hours=3),
+    walkin=False,
+    room=None,
+    state=ReservationState.CHECKED_OUT,
+    users=[user_data.root],
+    seats=[],
+)
+
+checked_out_reservation_5 = Reservation(
+    id=13,
+    start=now - timedelta(hours=4),
+    end=now - timedelta(hours=3),
+    created_at=now - timedelta(hours=4),
+    updated_at=now - timedelta(hours=3),
+    walkin=False,
+    room=None,
+    state=ReservationState.CHECKED_OUT,
+    users=[user_data.user],
+    seats=[],
+)
+
 
 def insert_fake_data(session: Session):
-    entity = ReservationEntity.from_model(current_reservation)
-    session.add(entity)
-    session.commit()
+    entity1 = ReservationEntity.from_model(current_reservation, session)
+    session.add(entity1)
+    entity2 = ReservationEntity.from_model(checked_out_reservation_1, session)
+    session.add(entity2)
+    entity3 = ReservationEntity.from_model(checked_out_reservation_2, session)
+    session.add(entity3)
+    entity4 = ReservationEntity.from_model(checked_out_reservation_3, session)
+    session.add(entity4)
+    entity5 = ReservationEntity.from_model(checked_out_reservation_4, session)
+    session.add(entity5)
+    entity6 = ReservationEntity.from_model(checked_out_reservation_5, session)
+    session.add(entity6)
+
 
 @pytest.fixture(autouse=True)
 def fake_data_fixture(session: Session):
--- backend/test/services/signage_test.py
+++ backend/test/services/signage_test.py
@@ -5,6 +5,8 @@ import pytest
 
 from sqlalchemy.orm import Session
 
+from backend.models.public_user import PublicUser
+
 from ...models import SignageOverviewFast, SignageOverviewSlow
 from ...services import SignageService
 
@@ -30,6 +32,7 @@ from .office_hours.office_hours_data import (
     fake_data_fixture as insert_order_8,
 )
 from .signage_data import fake_data_fixture as insert_order_9
+from .articles.article_data import fake_data_fixture as insert_order_10
 
 
 #  Import the fake model data in a namespace for test assertions
@@ -37,21 +40,44 @@ from .coworking import seat_data
 from . import room_data, user_data
 from .coworking.reservation import reservation_data
 from .office_hours import office_hours_data
+from .articles import article_data
+from .event import event_test_data
+from . import signage_data
+
 
 __authors__ = ["Will Zahrt", "Andrew Lockard"]
 __copyright__ = "Copyright 2024"
 __license__ = "MIT"
 
+
 def test_get_fast_data(signage_svc: SignageService):
     fast_data = signage_svc.get_fast_data()
+
     assert len(fast_data.active_office_hours) == 1
     assert (
         fast_data.active_office_hours[0].id
         == office_hours_data.comp_110_current_office_hours.id
     )
-    available_rooms = [room.id for room in room_data.rooms]
-    assert room_data.pair_a.id not in available_rooms
+
+    assert room_data.pair_a.id not in fast_data.available_rooms
 
     available_seats = [seat for seat in fast_data.seat_availability if seat.reservable]
     assert seat_data.monitor_seat_01 not in available_seats
     assert seat_data.monitor_seat_11 not in available_seats
+
+
+def test_get_slow_data(signage_svc: SignageService):
+    slow_data = signage_svc.get_slow_data()
+
+    assert article_data.announcement not in slow_data.newest_news
+    assert len(slow_data.newest_news) == 2
+
+    assert len(slow_data.events) == 3
+
+    assert len(slow_data.top_users) == 3
+    assert slow_data.top_users[0].id == user_data.ambassador.id
+    assert slow_data.top_users[1].id == user_data.root.id
+    assert slow_data.top_users[2].id == user_data.user.id
+
+    assert len(slow_data.announcement_titles) == 1
+    assert slow_data.announcement_titles[0] == "Sample Announcement"
