a6863e5e847166be53cb05986732c2bff95d6496
Nathan Kelete <97571116+nathankelete@users.noreply.github.com>
2024-11-04 16:28:15 -0500

    Update document.py, added author

--- backend/models/advising/document.py
+++ backend/models/advising/document.py
@@ -1,5 +1,10 @@
 """Pydantic models for the `Document` entity."""
 
+
+__author__ = ["Nathan Kelete"]
+__copyright__ = "Copyright 2023"
+__license__ = "MIT"
+
 from pydantic import BaseModel
 
 class Document(BaseModel):
