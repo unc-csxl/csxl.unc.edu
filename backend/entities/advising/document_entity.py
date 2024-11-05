26abb4600a28173083479a66c67f93be0c3fac57
nathankelete <nkelete@unc.edu>
2024-11-05 00:59:43 -0500

    Add entities and models for documents and sections. Will have to double check full text search capability.

--- backend/entities/advising/document_entity.py
+++ backend/entities/advising/document_entity.py
@@ -4,29 +4,26 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .entity_base import EntityBase
 from ...models.advising import document
 from backend.models.document import DocumentEnum
+from document_section_entity import SectionEntity
+from ...models.advising.document_details import DocumentDetails
 
 class DocumentEntity(EntityBase):
     __tablename__ = "document"
 
-    # Unique ID for the document
+    # Unique ID for the news post
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
     # Title of the document
     title: Mapped[str] = mapped_column(String, nullable=False)
 
-    # Tsvector for full-text search
-    tsv_content: Mapped[str] = mapped_column(
-        "tsv_content", 
-        type_=String, 
-        index=True,
-        nullable=False,
-    )
+    # Content of the document
+    content: Mapped[str] = mapped_column(String, nullable=False) # Might have to change later based on full text search
 
-    # Create a GIN index on the tsv_content column
-    __table_args__ = (
-        Index('ix_document_tsv_content', tsv_content, postgresql_using='gin'),
+    # NOTE: This field establishes a one-to-many relationship between the documents and sections table.
+    sections: Mapped[list["SectionEntity"]] = relationship(
+        back_populates="document", cascade="all,delete"
     )
-
+   
 
     @classmethod
     def from_model(cls, model: document) -> "DocumentEntity":
@@ -40,11 +37,8 @@ class DocumentEntity(EntityBase):
             DocumentEntity: The entity.
         """
         return cls(
-            id=document.id,
-            title=document.title,
-            content=document.content,
-            tsv_content=func.to_tsvector('english', model.content),
-            type=document.type,
+            id=model.id,
+            title=model.title
         )
     def to_model(self) -> document:
         """
@@ -55,5 +49,20 @@ class DocumentEntity(EntityBase):
         """
         return document(
             id=self.id,
-            title=self.title
+            title=self.title,
         )
+    def to_details_model(self) -> DocumentDetails:
+        """
+        Converts a `DocumentEntity` object into a `DocumentDetails` model object
+
+        Returns:
+            DocumentDetails: `DocumentDetails` object from the entity
+        """
+        return DocumentDetails(
+            id=self.id,
+            title=self.title,
+            content=self.content,
+            document_id=self.document_id,
+            sections=[section.to_overview_model() for section in self.sections],
+        )
+
