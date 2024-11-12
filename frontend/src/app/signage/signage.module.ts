be18419bf42843efb698407bc53f17aeafbfaa38
Andrew Lockard <alockard@unc.edu>
2024-11-12 13:50:06 -0500

    Occupancy in Component, Background Color, Title
    Added the background color and changed the title style. Also added
    some padding to the whole page as well as fixed title size.

--- frontend/src/app/signage/signage.module.ts
+++ frontend/src/app/signage/signage.module.ts
@@ -14,29 +14,30 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 // Angular Material Modules
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { OrganizationRoutingModule } from '../organization/organization-routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { SignageRoutingModule } from './signage-routing.module';
 import { SignageComponent } from './signage.component';
+import { OccupancyWidget } from './widgets/occupancy/occupancy.widget';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [SignageComponent],
+  declarations: [
+    SignageComponent,
+
+    // Widgets
+    OccupancyWidget
+  ],
   imports: [
     SignageRoutingModule,
     CommonModule,
@@ -44,21 +45,15 @@ import { SignageComponent } from './signage.component';
     MatTableModule,
     MatCardModule,
     MatDialogModule,
-    MatButtonModule,
     MatSelectModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatPaginatorModule,
     MatListModule,
-    MatAutocompleteModule,
-    FormsModule,
-    ReactiveFormsModule,
     MatIconModule,
     MatTooltipModule,
-    OrganizationRoutingModule,
     MatSidenavModule,
     MatToolbarModule,
-    RouterModule
+    RouterModule,
+    SharedModule
   ]
 })
 export class SignageModule {}
