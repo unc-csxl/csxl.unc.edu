57b56e2f51fd8af0dc3f940614f8e236a54c7dec
Andrew Lockard <alockard@unc.edu>
2025-01-23 19:00:32 -0500

    Rework Leaderbaord to add pagination

--- frontend/src/app/signage/widgets/leaderboard/leaderboard.widget.ts
+++ frontend/src/app/signage/widgets/leaderboard/leaderboard.widget.ts
@@ -2,11 +2,12 @@
  * The Leaderboard Card displays top ten checked in users in a paginated list on the CSXL TV.
  *
  * @author Will Zahrt <wzahrt@unc.edu>
+ * @author Andrew Lockard <andrew.lockard15@gmail.com>
  * @copyright 2024
  * @license MIT
  */
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PublicProfile } from 'src/app/profile/profile.service';
 import { SignageService } from '../../signage.service';
 
@@ -15,32 +16,37 @@ import { SignageService } from '../../signage.service';
   templateUrl: './leaderboard.widget.html',
   styleUrls: ['./leaderboard.widget.css']
 })
-export class LeaderboardWidget implements OnInit {
+export class LeaderboardWidget implements OnChanges {
   /** Inputs and outputs go here */
-  @Input() profiles!: PublicProfile[];
-  top_five: PublicProfile[] = [];
-  lower_five: PublicProfile[] = [];
-  @Input() profile!: PublicProfile;
+  @Input() profiles: PublicProfile[] = []; // Should be a max of 10 elements
+  shown_indicies: number[] = [
+    ...Array(this.profiles.length > 5 ? 5 : this.profiles.length).keys()
+  ];
 
-  /** Constructor */
-  constructor(public signageService: SignageService) {
-    console.log(
-      'this is the profiles: ' + this.signageService.slowData().top_users
-    );
-    console.log(
-      'this is the top_five: ' + this.signageService.slowData().top_users
-    );
-    this.top_five = this.signageService.slowData().top_users.slice(5);
-    // this.lower_five = this.profiles.slice(6, 10);
-    // console.log(this.top_five);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes['profiles'] &&
+      !this.shown_indicies.includes(5) &&
+      this.shown_indicies.length < 5
+    ) {
+      // Only run this if there is less than 5 on the leaderboard right now
+      this.shown_indicies = [
+        ...Array(this.profiles.length > 5 ? 5 : this.profiles.length).keys()
+      ];
+    }
   }
 
-  ngOnInit(): void {
-    console.log(
-      'this is the profiles: ' + this.signageService.slowData().top_users
-    );
-    console.log(
-      'this is the top_five: ' + this.signageService.slowData().top_users
-    );
+  switch_page() {
+    if (this.shown_indicies.includes(1)) {
+      // Generates an array of numbers 5 to length of profile
+      this.shown_indicies = [
+        ...Array(
+          this.profiles.length >= 10 ? 5 : this.profiles.length - 5
+        ).keys()
+      ].map((i) => i + 5);
+    } else {
+      // At this point we can assume that the array is larger than 5 elements
+      this.shown_indicies = [...Array(5).keys()];
+    }
   }
 }
