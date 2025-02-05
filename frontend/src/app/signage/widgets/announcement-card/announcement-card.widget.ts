703c046f8a498ca0110b473a50f008279ea790ff
ItIsAndrewL <andrew.lockard15@gmail.com>
2025-02-04 19:36:32 -0500

    Announcements shown, not styled yet

--- frontend/src/app/signage/widgets/announcement-card/announcement-card.widget.ts
+++ frontend/src/app/signage/widgets/announcement-card/announcement-card.widget.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+/**
+ * Annoucement Widget to Display Annoucement Headlines on the CSXL Signage
+ *
+ * @author Andrew Lockard <andrew.lockard15@gmail.com>
+ * @license MIT
+ * @copyright 2025
+ *
+ */
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  OnDestroy
+} from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'announcement-card',
   templateUrl: 'announcement-card.widget.html',
   styleUrls: ['announcement-card.widget.css']
 })
-export class AnnouncementCardWidget {
-  // Component logic goes here
+export class AnnouncementCardWidget implements OnChanges, OnDestroy {
+  @Input() announcements!: string[];
+  announcement_to_display = 0;
+  rotating_subscription: Subscription | null = null;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['annoucements']) {
+      this.announcement_to_display = 0;
+      // Handle rotating between multiple announcements
+      if (this.announcements.length > 1 && this.rotating_subscription == null) {
+        this.rotating_subscription = timer(0, 30000).subscribe(() => {
+          this.announcement_to_display =
+            (this.announcement_to_display + 1) % this.announcements.length;
+        });
+      } else if (
+        this.announcements.length < 1 &&
+        this.rotating_subscription != null
+      ) {
+        this.rotating_subscription.unsubscribe();
+        this.rotating_subscription = null;
+      }
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.rotating_subscription) {
+      this.rotating_subscription.unsubscribe();
+    }
+  }
 }
