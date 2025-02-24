b00832414b935fa9e4e822806d9e1cf4b6b3511b
Kris Jordan <kris@cs.unc.edu>
2025-02-23 20:58:54 -0500

    Add simple proof-of-concept WebSocket test for production purposes (#705)

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SlackInviteBox } from '../navigation/widgets/slack-invite-box/slack-invite-box.widget';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit, OnDestroy {
+  private socket$: WebSocketSubject<any>;
+  private socketSubscription?: Subscription;
+
   public static Route = {
     path: 'about',
     title: 'About the XL',
     component: AboutComponent
   };
 
-  constructor(protected dialog: MatDialog) {}
+  constructor(protected dialog: MatDialog) {
+    const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+    const url = `${protocol}://${window.location.host}/ws/echo`;
+    this.socket$ = webSocket({ url });
+  }
+
+  ngOnInit(): void {
+    let counter = 0;
+    this.socketSubscription = this.socket$.subscribe((message) => {
+      console.log(`Received: ${message}`);
+      setTimeout(() => this.socket$.next(`counter: ${counter++}`), 1000);
+    });
+    this.socket$.next('onInit');
+  }
+
+  ngOnDestroy(): void {
+    this.socketSubscription?.unsubscribe();
+  }
 
   onSlackInviteClick(): void {
     const dialogRef = this.dialog.open(SlackInviteBox, {
