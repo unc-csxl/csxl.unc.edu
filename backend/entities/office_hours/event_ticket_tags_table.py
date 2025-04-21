b7d72c8b3794a65e57dac679dff856a2412346d7
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2025-04-21 10:46:52 -0400

    (task:768) Create ticket tag entities and models

---
+++ backend/entities/office_hours/event_ticket_tags_table.py
@@ -0,0 +1,17 @@
+"""Definition of SQLAlchemy table-backed object mapping entity for Office Hour tickets."""
+
+from sqlalchemy import Column, ForeignKey, Table
+from ..entity_base import EntityBase
+
+
+__authors__ = ["Jade Keegan"]
+__copyright__ = "Copyright 2025"
+__license__ = "MIT"
+
+# Association table between OfficeHoursTicket and OfficeHoursTicketTag
+event_ticket_tags_table = Table(
+    "office_hours__event_ticket_tags",
+    EntityBase.metadata,
+    Column("ticket_id", ForeignKey("office_hours__ticket.id"), primary_key=True),
+    Column("member_id", ForeignKey("office_hours__ticket_tag.id"), primary_key=True),
+)
---
+++ backend/models/office_hours/ticket_tag.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+from .office_hours import CourseSite
+
+__authors__ = ["Jade Keegan"]
+__copyright__ = "Copyright 2025"
+__license__ = "MIT"
+
+
+class NewOfficeHoursTicketTag(BaseModel):
+    """
+    Pydantic model to represent a new ticket tag.
+
+    This model is based on the `OfficeHoursTicketTagEntity` model, which defines the shape
+    of the `OfficeHoursTicketTag` database in the PostgreSQL database.
+    """
+
+    name: str
+    course_site_id: int
+
+
+class OfficeHoursTicketTag(NewOfficeHoursTicketTag):
+    """
+    Pydantic model to represent an `OfficeHoursTicketTag`.
+
+    This model is based on the `OfficeHoursTicketTagEntity` model, which defines the shape
+    of the `OfficeHoursTicketTag` database in the PostgreSQL database.
+    """
+
+    id: int
+
+class OfficeHoursTicketTagDetails(OfficeHoursTicketTag):
+    """
+    Pydantic model to represent an `OfficeHoursTicketTag` with details.
+
+    This model is based on the `OfficeHoursTicketTagEntity` model, which defines the shape
+    of the `OfficeHoursTicketTag` database in the PostgreSQL database.
+    """
+
+    course_site: CourseSite
