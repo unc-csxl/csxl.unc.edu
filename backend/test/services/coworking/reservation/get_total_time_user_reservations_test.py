8de3a19046b717ee61ba1e0e6774d8604df7f4bf
Kris Jordan <kris@cs.unc.edu>
2026-04-14 23:51:14 -0400

    test: migrate coworking reservation total-time tests

--- backend/test/services/coworking/reservation/get_total_time_user_reservations_test.py
+++ backend/test/services/coworking/reservation/get_total_time_user_reservations_test.py
@@ -1,37 +1,11 @@
 """Tests for ReservationService#get_map_reservations_for_date and helper functions."""
 
-from backend.models.coworking.availability import RoomState
-from backend.models.coworking.reservation import ReservationState
-from datetime import date
+import pytest
+from sqlalchemy.orm import Session
 
 from .....services.coworking import ReservationService
-
-# Imported fixtures provide dependencies injected for the tests as parameters.
-# Dependent fixtures (seat_svc) are required to be imported in the testing module.
-from ..fixtures import (
-    reservation_svc,
-    permission_svc,
-    seat_svc,
-    policy_svc,
-    operating_hours_svc,
-)
-
-from ..time import *
-
-# Import the setup_teardown fixture explicitly to load entities in database.
-# The order in which these fixtures run is dependent on their imported alias.
-# Since there are relationship dependencies between the entities, order matters.
-from ...core_data import user_data
-from ...core_data import setup_insert_data_fixture as insert_order_0
-from ..operating_hours_data import fake_data_fixture as insert_order_1
-from ...room_data import fake_data_fixture as insert_order_2
-from ..seat_data import fake_data_fixture as insert_order_3
-from .reservation_data import fake_data_fixture as insert_order_4
-
-# Import the fake model data in a namespace for test assertions
-from ...core_data import user_data
-from .. import seat_data
-from . import reservation_data
+from .scenario import arrange_standard_reservation_scenario, make_reservation_service
+from ..time import time_data
 
 __authors__ = [
     "Yuvraj Jain",
@@ -40,18 +14,28 @@ __copyright__ = "Copyright 2024"
 __license__ = "MIT"
 
 
-def test_get_total_time_user_reservations_student(reservation_svc: ReservationService):
-    hours = reservation_svc.get_total_time_user_reservations(user_data.user)
+pytestmark = pytest.mark.integration
+
+
+def test_get_total_time_user_reservations_student(session: Session):
+    scenario = arrange_standard_reservation_scenario(session, time_data())
+    reservation_svc = make_reservation_service(session)
+
+    hours = reservation_svc.get_total_time_user_reservations(scenario.user)
     assert hours == "4.5"
 
 
-def test_get_total_time_user_reservations_ambassador(
-    reservation_svc: ReservationService,
-):
-    hours = reservation_svc.get_total_time_user_reservations(user_data.ambassador)
+def test_get_total_time_user_reservations_ambassador(session: Session):
+    scenario = arrange_standard_reservation_scenario(session, time_data())
+    reservation_svc = make_reservation_service(session)
+
+    hours = reservation_svc.get_total_time_user_reservations(scenario.ambassador)
     assert hours == "6"
 
 
-def test_get_total_time_user_reservations_root(reservation_svc: ReservationService):
-    hours = reservation_svc.get_total_time_user_reservations(user_data.root)
+def test_get_total_time_user_reservations_root(session: Session):
+    scenario = arrange_standard_reservation_scenario(session, time_data())
+    reservation_svc = make_reservation_service(session)
+
+    hours = reservation_svc.get_total_time_user_reservations(scenario.root)
     assert hours == "6"
